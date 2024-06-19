d9061751232a178bdf9f8d42c9ea60b33b8c7e04
Pawel <rayven@protonmail.com>
2024-06-20 00:02:55 +0200

    fix sync/send issues

--- src/key_signature_set.rs
+++ src/key_signature_set.rs
@@ -1,3 +1,4 @@
+/// KeySignatureSet is a trait that defines the public key and signature types.
 pub trait KeySignatureSet {
     type PublicKey: AsRef<[u8]>;
     type Signature: AsRef<[u8]>;
--- src/storage.rs
+++ src/storage.rs
@@ -24,7 +24,7 @@ pub trait KeyCreate<K: KeySignatureSet> {
 /// KeySign trait is a trait that is used to sign a hash with a private key located in a key store. The method return a [`Signer`] object.
 pub trait KeySign<K: KeySignatureSet> {
     type KeyID;
-    fn signer<'a>(&'a self, key_id: &'a Self::KeyID) -> impl Signer<K> + 'a;
+    fn signer(&self, key_id: Self::KeyID) -> impl Signer<K>;
 }
 
 /// KeyDelete trait is a trait that is used to delete a key pair from the key store.
