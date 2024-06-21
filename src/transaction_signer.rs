655a8e7f7569c42f10b82b37cba102c4be69f517
Pawel <rayven@protonmail.com>
2024-06-21 10:26:57 +0200

    fix: signature type in TransactionSigner

--- src/transaction_signer.rs
+++ src/transaction_signer.rs
@@ -1,16 +1,21 @@
+use iota_types::crypto::Signature as IotaSignature;
 use iota_types::transaction::TransactionData;
 
 use crate::{hash::ToHash, key_signature_set::KeySignatureSet, prelude::Signer};
 
 /// Transaction signer trait is a trait is a helper auto-trait that allows to sign IOTA Transactions
 /// This trait requires to `transaction_helper` feature to be enabled.
-/// Because the `TransactionData` is domain specific, the `transaction_helper` feature is disabled by default.
+/// Because the `TransactionData` and `Signature` are domain specific, the `transaction_helper` feature
+/// is disabled by default.
 pub trait TransactionSigner<K: KeySignatureSet>: Signer<K> {
     async fn sign_transaction(
         &self,
         transaction: &TransactionData,
-    ) -> Result<K::Signature, anyhow::Error> {
+    ) -> Result<iota_types::crypto::Signature, anyhow::Error>
+    where
+        K::Signature: Into<IotaSignature>,
+    {
         let hash = transaction.calculate_data_hash();
-        self.sign(hash).await
+        self.sign(hash).await.map(Into::into)
     }
 }
