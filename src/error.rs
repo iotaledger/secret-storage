1e3db13c1da5581f482377988fe697a8b72503e5
Pawel Iwan <pawel.ivan@iota.org>
2025-09-03 10:29:36 +0200

    feat: no-std support

--- src/error.rs
+++ src/error.rs
@@ -3,8 +3,12 @@
 
 use thiserror::Error;
 
-pub type Result<T, E = Error> = std::result::Result<T, E>;
+#[cfg(not(feature = "std"))]
+extern crate alloc;
 
+pub type Result<T, E = Error> = core::result::Result<T, E>;
+
+#[cfg(feature = "std")]
 #[derive(Debug, Error)]
 pub enum Error {
     #[error("key with ID {0} could not be found")]
@@ -15,4 +19,17 @@ pub enum Error {
     InvalidOptions,
     #[error(transparent)]
     Other(anyhow::Error),
+}
+
+#[cfg(not(feature = "std"))]
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("key with ID {0} could not be found")]
+    KeyNotFound(alloc::string::String),
+    #[error("unable to communicate with key store: {0}")]
+    StoreDisconnected(alloc::string::String),
+    #[error("failed to generate key with provided options")]
+    InvalidOptions,
+    #[error("other error: {0}")]
+    Other(alloc::string::String),
 }
