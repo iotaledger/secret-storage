3656d399cb927bf07656d263d5c136815142d0b9
Sebastian Wolfram <wulfraem@users.noreply.github.com>
2026-03-25 14:48:03 +0100

    split examples to allow further extension

--- adapters/aws-kms-adapter/bindings/wasm/aws_kms_adapter_wasm/examples/src/main.ts
+++ adapters/aws-kms-adapter/bindings/wasm/aws_kms_adapter_wasm/examples/src/main.ts
@@ -1,19 +1,21 @@
 // Copyright 2020-2025 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import { helloTs } from "@iota/aws_kms_adapter_wasm/node";
-import { AwsKmsStorage } from "@iota/aws_kms_adapter_wasm/node/aws_kms_adapter_wasm";
+import { testStorage } from "./storage";
 
-const ACCESS_KEY = process.env.ACCESS_KEY || "foo";
-const SECRET_ACCESS_KEY = process.env.SECRET_ACCESS_KEY || "bar";
+export async function main(example?: string) {
+    // Extract example name.
+    const argument = example ?? process.argv?.[2]?.toLowerCase();
+    if (!argument) {
+        throw "Please specify an example name, e.g. 'storage'";
+    }
 
-export async function main() {
-    console.log(helloTs());
-
-    const storage = await AwsKmsStorage.create("eu-west-1", ACCESS_KEY, SECRET_ACCESS_KEY);
-
-    const pk = await storage.public_key("7a1b6dfb-df9c-4a6b-b3c4-29c028c82817");
-    console.dir(pk);
+    switch (argument) {
+        case "storage":
+            return await testStorage();
+        default:
+            throw "Unknown example name: '" + argument + "'";
+    }
 }
 
 main()
