e0282dbb266bdf6d231474d754d244c6568596cc
Sebastian Wolfram <wulfraem@users.noreply.github.com>
2026-05-29 10:27:32 +0200

    remove unused raw scheme

--- core/secret-storage/src/lib.rs
+++ core/secret-storage/src/lib.rs
@@ -10,14 +10,3 @@ pub use error::*;
 pub use signature_scheme::*;
 pub use signer::*;
 pub use storage::*;
-
-#[cfg(feature = "signature-scheme-raw")]
-#[path = ""]
-mod signature_scheme_raw {
-    mod signature_scheme_raw;
-    mod storage_update;
-    pub use signature_scheme_raw::*;
-    pub use storage_update::*;
-}
-#[cfg(feature = "signature-scheme-raw")]
-pub use signature_scheme_raw::*;
